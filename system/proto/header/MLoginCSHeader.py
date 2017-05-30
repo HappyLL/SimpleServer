6605d113448dcaae18ef28e51c5f9ffed477c339
Happyli <18868803201@163.com>
2017-05-30 21:06:48 +0800

    协议编码解码调试

---
+++ system/proto/header/MLoginCSHeader.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from system.proto.Header import Header
+
+class MLoginCSHeader(Header):
+	def __init__(self, hid):
+		super(MLoginCSHeader, self).__init__(hid)
+		self._add_val('name', '131123123124124', 's')
+		self._add_val('id', 1, 'i')
+		self._add_val('height', '182', 's')
+		self._add_val('sex', 'male', 's')
+		self._add_val('age', 18, 'i')
+
+
+
+
+if __name__ == '__main__':
+	login = MLoginCSHeader(0)
+	login.height = '189'
+	ret = login.header_encode()
+	print 'encode_ret ', ret
+
+	ret = login.header_decode(ret)
+	print  login.height
